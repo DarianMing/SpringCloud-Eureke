76474635e8d7f16471166c461ea875087dbce872
liming <2452208646@qq.com>
2018-12-17 22:21:39 +0800

    '增加消费者rest和Ribbon'

---
+++ RibbonConsumer/src/main/java/com/lm/demo/controller/ConsumerController.java
@@ -0,0 +1,4 @@
+package com.lm.demo.controller;
+
+public class ConsumerController {
+}
---
+++ ServiceProvider-3/src/main/java/com/lm/demo/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.lm.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @Value("${server.port")
+    String port;
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home () {
+        return "hello World , port: " + port;
+    }
+}
