96698e6c2696990a4035b85ef0eac6d761e626d8
DarainMing <743310059@qq.com>
2018-12-27 01:45:36 +0800

    '分布式配置中心SpringCloudConfig中使用Refresh'

--- ServiceProvider-3/src/main/java/com/lm/demo/controller/HomeController.java
+++ ServiceProvider-3/src/main/java/com/lm/demo/controller/HomeController.java
@@ -1,12 +1,18 @@
 package com.lm.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
+//@RefreshScope要加在声明@Controller声明的类上，
+//否则refresh之后Conroller拿不到最新的值，会默认调用缓存
+@RefreshScope
 @RestController
 public class HomeController {
+
+    @Value("${content}")
+    String content;
 
     @Value("${server.port}")
     String port;
@@ -14,6 +20,6 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String home () {
-        return "hello World , port: " + port;
+        return "hello World , port: " + port + " ,content=" + content;
     }
 }
