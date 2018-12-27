a85291aad902f74c257050682552a04f0125dfa9
liming <2452208646@qq.com>
2018-12-27 16:42:17 +0800

    'SpringCloudBus消息总线集成（RabbitMQ）'

--- ConfigServerEureka/src/main/java/com/lm/demo/DemoApplication.java
+++ ConfigServerEureka/src/main/java/com/lm/demo/DemoApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
+//https://www.souyunku.com/2017/12/24/spring-cloud-config-bus-rabbitMQ/#spring-cloud-bus
+//https://my.oschina.net/u/2263272/blog/1634010
+//https://357029540.iteye.com/blog/2432558
+//https://github.com/422518490/orderSystem
+//postman 实现半自动刷新请求地址(POST)http://localhost:8888/actuator/bus-refresh
 @EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
